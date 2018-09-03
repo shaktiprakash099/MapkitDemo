9fc283b517717ad4a1fe6ba97915cbd4e7bcceb6
shaktiprakash <shaktiprakash@globussoft.in>
2018-09-03 13:41:10 +0530

    direction added

--- MapKitDemo/ViewController.swift
+++ MapKitDemo/ViewController.swift
@@ -11,17 +11,21 @@ import MapKit
 import CoreLocation
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var Gobutton: UIButton!
+    
     let locationManager = CLLocationManager()
     let regionInMeteers : Double = 10000
     var previousLocation: CLLocation?
+    var directionsArray: [MKDirections] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       mapView.delegate = self
+        mapView.delegate = self
         checkLocationServices()
+        Gobutton.layer.cornerRadius = Gobutton.frame.size.height/2
     }
     
     
@@ -54,14 +58,73 @@ class ViewController: UIViewController {
         return CLLocation(latitude: latitutude, longitude: longitude)
         
     }
+    
+    func getDirections() {
+        guard let location = locationManager.location?.coordinate else {
+            //TODO : inform user we don't have their current locations
+            print("We dont have user locations ")
+            return
+        }
+        let request = createDirectionsRequest(from: location)
+        let directions = MKDirections(request: request)
+        resetMapView(withNew: directions)
+        directions.calculate { (response , error) in
+            
+            
+            
+            if  let errors = error {
+                print("error  occured ")
+                print("\(errors)")
+                
+            }
+            guard let response = response else {
+                print("Response not available")
+                return
+            }
+            
+            
+            
+            for route in response.routes {
+                self.mapView.add(route.polyline)
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+            
+        }
+    }
+    
+    
+    func createDirectionsRequest(from coordinate: CLLocationCoordinate2D)-> MKDirectionsRequest{
+        
+        let destinationCoorinate                 = getCenterLocation(for: mapView).coordinate
+        let startingLocation                         = MKPlacemark(coordinate: coordinate)
+        let destination                                   = MKPlacemark(coordinate: destinationCoorinate)
+        let request                                           = MKDirectionsRequest()
+        request.source                                     = MKMapItem(placemark: startingLocation)
+        request.destination                            = MKMapItem(placemark: destination)
+        request.transportType                      = .automobile
+        request.requestsAlternateRoutes   = true
+        return request
+    }
+    
+    func resetMapView(withNew directions:MKDirections){
+        mapView.removeOverlays(mapView.overlays)
+        directionsArray.append(directions)
+        let _ = directionsArray.map{$0.cancel()}
+        
+    }
+    
+    @IBAction func goDirectionButtonTapped(_ sender: Any) {
+        getDirections()
+    }
+    
     func checkLocationAuthorization(){
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
             startTrackingUserLocation()
-        
+            
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
-        
+            
         case .restricted:
             //show alerat message
             break
@@ -70,12 +133,12 @@ class ViewController: UIViewController {
             break
         case .authorizedAlways:
             // authorization
-           break
+            break
         }
         
         
     }
-
+    
     
     func startTrackingUserLocation (){
         mapView.showsUserLocation = true
@@ -84,7 +147,7 @@ class ViewController: UIViewController {
         previousLocation = getCenterLocation(for: mapView)
         
     }
-
+    
 }
 
 extension ViewController:CLLocationManagerDelegate {
@@ -123,6 +186,13 @@ extension ViewController : MKMapViewDelegate {
     }
     
     
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
+        renderer.strokeColor = .blue
+        
+        return renderer
+    }
     func getCompleteAddressofPlacemark(placemark:CLPlacemark) -> String{
         
         let streetNumber = placemark.postalCode ?? ""
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 #  MapKitDemo
-Simple Map Kit  demo  here we will be  getting an address from a coordinate using reverse geolocating. We'll display that address on the screen and updated it as the user moves the map.
+Simple Map Kit  demo  here we will be  getting an address from a coordinate using reverse geolocating. We'll display that address on the screen and updated it as the user moves the map.Also user can user can trace direction polylines and add overlays on maps .
 <BR>
 [![Screen_Shot_2018-08-28_at_4.57.38_PM.png](https://s15.postimg.cc/b1hieyh5n/Screen_Shot_2018-08-28_at_4.57.38_PM.png)](https://postimg.cc/image/50jthvujb/)
 [![Screen_Shot_2018-08-28_at_4.56.21_PM.png](https://s15.postimg.cc/3ms6m15ej/Screen_Shot_2018-08-28_at_4.56.21_PM.png)](https://postimg.cc/image/n4mu1z2c7/)
+[![Screen_Shot_2018-09-03_at_1.38.50_PM.png](https://s15.postimg.cc/s8g177gbv/Screen_Shot_2018-09-03_at_1.38.50_PM.png)](https://postimg.cc/image/r65uonxif/)
